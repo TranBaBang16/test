4320b830571494f02ac65bf438e3edc1d86eb8c5
TranBaBang16 <tranbabang16@gmail.com>
2023-07-17 21:21:14 +0700

    by tranbang

--- hi.c
+++ hi.c
@@ -1,13 +1,135 @@
-#include<stdio.h>
-void test(){
+#include <stdio.h>
+#include <string.h>
+void test()
+{
     printf("this is func \"test\"");
 };
-int sum(int a,int b){
-    return a+b;
+int sum(int a, int b)
+{
+    return a + b;
 };
-int main() {
-
-printf("%d\n",sum(14,15));
-test();
+int groupedBits(int n)
+{
+    int count = 0;
+    int rem = 0;
+    while (n != 0)
+    {
+        if ((n % 2 != rem) && (n % 2 == 1))
+        {
+            count = count + 1;
+        };
+        rem = n % 2;
+        n = n / 2;
+    };
+    return count;
+}
+char *electronicScreen(char *s)
+{
+    int i = 0;
+    char *result;
+    int count = 0;
+    char  numberzer[] = "01011111",
+            numberone[] = "00000101",
+            numbertwo[]= "01110110",
+            numberthr[] = "01110101",
+            numberfou[] = "00101101",
+            numberfiv[] = "01111001",
+            numbersix[] = "01111011",
+            numbersev[] = "01000101",
+            numbereig[] = "01111111",
+            numbernin[] = "01111101";
+    while (*(s + i) != NULL)
+    {
+        char* temp;
+        for (int j = 0; j < 8; j++){
+            *(temp + j) = *(s + i + j);
+        }
+        *(temp + 8) = NULL;
+        if (strcmp(temp, numberzer) == 0)
+        {
+            *(result + count) = '0';
+            count++;
+            i+=8;
+            continue;
+        }
+        if (strcmp(temp, numberone) == 0)
+        {
+            *(result + count) = '1';
+            i+=8;
+            count++;
+            continue;
+        }
+        if (strcmp(temp, numbertwo) == 0)
+        {
+            *(result + count) = '2';
+            count++;
+            i+=8;
+            continue;
+        }
+        if (strcmp(temp, numberthr) == 0)
+        {
+            *(result + count) = '3';
+            i+=8;
+            count++;
+            continue;
+        }
+        if (strcmp(temp, numberfou) == 0)
+        {
+            *(result + count) = '4';
+            count++;
+            i+=8;
+            continue;
+        }
+        if (strcmp(temp, numberfiv) == 0)
+        {
+            *(result + count) = '5';
+            count++;
+            i+=8;
+            continue;
+        }
+        if (strcmp(temp, numbersix) == 0)
+        {
+            *(result + count) = '6';
+            i+=8;
+            count++;
+            continue;
+        }
+        if (strcmp(temp, numbersev) == 0)
+        {
+            *(result + count) = '7';
+            i+=8;
+            count++;
+            continue;
+        }
+        if (strcmp(temp, numbereig) == 0)
+        {
+            *(result + count) = '8';
+            i+=8;
+            count++;
+            continue;
+        }
+        if (strcmp(temp, numbernin) == 0)
+        {
+            *(result + count) = '9';
+            i+=8;
+            count++;
+            continue;
+        }
+    }
+    count++;
+    *(result + count) = NULL;
+    return result;
+}
+int main()
+{
+    // char *s = electronicScreen("00000101");
+    // int i = 0;
+    // while (*(s + i) != NULL)
+    // {
+    //     printf("%c", *(s + i));
+    //     i++;
+    // }
+    char *s="tra\0";
+    printf("%c",*(s+4));
     return 0;
 }
